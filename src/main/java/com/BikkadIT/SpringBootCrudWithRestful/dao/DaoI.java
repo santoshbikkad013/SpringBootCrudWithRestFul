61591756f42f77550f491a1ac4ae47e421566ff2
gavalishubham94 <gavalishubham94@gmail.com>
2022-08-12 10:44:45 +0530

    SBCWR-11:declare method in daoImpl

--- src/main/java/com/BikkadIT/SpringBootCrudWithRestful/dao/DaoI.java
+++ src/main/java/com/BikkadIT/SpringBootCrudWithRestful/dao/DaoI.java
@@ -1,5 +1,11 @@
 package com.BikkadIT.SpringBootCrudWithRestful.dao;
 
+import java.util.List;
+
+import com.BikkadIT.SpringBootCrudWithRestful.model.Employee;
+
 public interface DaoI {
 
+	public Employee addEmployee(Employee employee);
+	public List<Employee> getAllEmployee();
 }
--- src/main/java/com/BikkadIT/SpringBootCrudWithRestful/dao/DaoImpl.java
+++ src/main/java/com/BikkadIT/SpringBootCrudWithRestful/dao/DaoImpl.java
@@ -1,5 +1,13 @@
 package com.BikkadIT.SpringBootCrudWithRestful.dao;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class DaoImpl implements DaoI{
+
+
 
 }
