e2bfe60b4cd6159502982c458a947bc7aa84bded
Dipak771996 <deepakpidurkar77@gmail.com>
2022-08-12 11:02:09 +0530

    SBCWR-10: provide implementation in serviceImpl Class

--- src/main/java/com/BikkadIT/SpringBootCrudWithRestful/service/ServiceImpl.java
+++ src/main/java/com/BikkadIT/SpringBootCrudWithRestful/service/ServiceImpl.java
@@ -1,5 +1,22 @@
 package com.BikkadIT.SpringBootCrudWithRestful.service;
 
+import java.util.List;
+
+import com.BikkadIT.SpringBootCrudWithRestful.model.Employee;
+
 public class ServiceImpl implements ServiceI {
 
+	@Override
+	public Employee addEmployee(Employee employee) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Employee> getAllEmployee() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
 }
