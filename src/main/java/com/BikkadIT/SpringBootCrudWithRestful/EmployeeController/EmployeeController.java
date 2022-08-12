d7c43c474b5faff2344beddbad534096e5d71955
Your Name <shendepoonam1997@gmail.com>
2022-08-12 10:54:03 +0530

    SBCWR-13:provide implementation for Controller layer

--- src/main/java/com/BikkadIT/SpringBootCrudWithRestful/EmployeeController/EmployeeController.java
+++ src/main/java/com/BikkadIT/SpringBootCrudWithRestful/EmployeeController/EmployeeController.java
@@ -1,18 +1,22 @@
 package com.BikkadIT.SpringBootCrudWithRestful.EmployeeController;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+
+import com.BikkadIT.SpringBootCrudWithRestful.model.Employee;
 
 @Controller
 public class EmployeeController
 {   
 	
-    public String addEmployee()
+    public Employee addEmployee(Employee employee)
     {
 		return null;
     	
     }
     
-    public String getAllEmployee()
+    public List<Employee>  getAllEmployee()
     {
 		return null;
     	
