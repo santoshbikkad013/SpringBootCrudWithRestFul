8324a463ce232cbc3f9ab68f30020f1ffc63c9ed
Your Name <shendepoonam1997@gmail.com>
2022-08-12 16:42:26 +0530

    SBCWR-13 : provide implementation for Controller layer

--- src/main/java/com/BikkadIT/SpringBootCrudWithRestful/EmployeeController/EmployeeController.java
+++ src/main/java/com/BikkadIT/SpringBootCrudWithRestful/EmployeeController/EmployeeController.java
@@ -3,6 +3,7 @@ package com.BikkadIT.SpringBootCrudWithRestful.EmployeeController;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.BikkadIT.SpringBootCrudWithRestful.model.Employee;
@@ -10,7 +11,7 @@ import com.BikkadIT.SpringBootCrudWithRestful.model.Employee;
 @RestController
 public class EmployeeController
 {   
-	
+	@GetMapping(value = "/addEmployee",produces = "application/json",consumes = "application/json")
     public ResponseEntity<Employee> addEmployee(Employee employee)
     {
 		return null;
