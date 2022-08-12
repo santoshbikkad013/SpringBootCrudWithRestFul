3b63eaf2b5c2d58dbc2673280551dbfa7e737eab
jadhavgajanan <gajananjadhav1020@gmail.com>
2022-08-12 10:54:12 +0530

    SBCWR-12 :provide implementation in daoImpl Class

--- src/main/java/com/BikkadIT/SpringBootCrudWithRestful/dao/DaoImpl.java
+++ src/main/java/com/BikkadIT/SpringBootCrudWithRestful/dao/DaoImpl.java
@@ -1,5 +1,21 @@
 package com.BikkadIT.SpringBootCrudWithRestful.dao;
 
+import java.util.List;
+
+import com.BikkadIT.SpringBootCrudWithRestful.model.Employee;
+
 public class DaoImpl implements DaoI{
+
+	@Override
+	public Employee addEmployee(Employee employee) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Employee> getAllEmployee() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
